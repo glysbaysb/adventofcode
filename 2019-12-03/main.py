cfc0a23a8c1cdae3721d097a8fabfa382927b4e9
Geert Ijewski <gm.ijewski@web.de>
2019-12-13 20:55:38 +0100

    i cheated on looked it up
    so i thought of building a x * y array and really tracing the paths.
    then i looked at some solutions and people just built a hashmap out of a
    coordinate class. that is much easier, so am gonna skip this

--- 2019-12-03/main.py
+++ 2019-12-03/main.py
@@ -1,1 +1,50 @@
 #!/bin/env python3
+
+def get_max_positions(movments):
+    maxX = 0
+    minX = 0
+    curX = 0
+
+    maxY = 0
+    minY = 0
+    curY = 0
+
+    for m in movments:
+        direction = m[0]
+        length = int(m[1:])
+
+        # move
+        if direction == 'L':
+            curX -= length
+        elif direction == 'R':
+            curX += length
+        elif direction == 'U':
+            curY += length
+        elif direction == 'D':
+            curY -= length
+
+        # check bounds
+        if maxX < curX:
+            maxX = curX
+        if minX > curX:
+            minX = curX
+        if maxY < curY:
+            maxY = curY
+        if minY > curY:
+            minY = curY
+
+
+    return maxX - minY, maxY - minY
+
+
+'''
+l1 = ['R8','U5','L5','D3']
+l2 = ['U7','R6','D4','L4']
+print(get_max_positions(l1))
+print(get_max_positions(l2))
+'''
+f = open('input')
+l1 = f.readline().split(',')
+l2 = f.readline().split(',')
+
+print(get_max_positions(l1), get_max_positions(l2))
