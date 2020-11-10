88c36d7865114abbe4013eef26485c6b9039cdfa
Geert Ijewski <gm.ijewski@web.de>
2020-11-10 21:39:48 +0100

    day 8 part 2

--- 2019-12-08/main.py
+++ 2019-12-08/main.py
@@ -8,16 +8,22 @@ LAYER_LENGTH = WIDTH * HEIGHT
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
 
-found0 = 9999
-foundChunk = ''
-
 for line in open('input'):
     allChunks = list(chunkstring(line, LAYER_LENGTH))
+    layers = []
     for chunk in allChunks:
         if chunk == '\n':
             continue
-        if chunk.count('0') < found0:
-            found0 = chunk.count('0')
-            foundChunk = chunk
-print(foundChunk)
-print(foundChunk.count('1') * foundChunk.count('2'))
+        layers.insert(len(layers) - 1, list(chunk))
+for h in range(HEIGHT):
+    for w in range(WIDTH):
+        for l in range(len(layers)):
+            if layers[l][h * WIDTH + w] == '2':
+                continue
+            # white output on black terminal so invert
+            if layers[l][h*WIDTH + w] == '0':
+                print(' ', end='')
+            else:
+                print('#', end='')
+            break
+    print()
