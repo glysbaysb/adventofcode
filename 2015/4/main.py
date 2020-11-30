3f6765a112617068f974347ad64ee39227a0ebad
Geert Ijewski <gm.ijewski@web.de>
2020-11-30 20:41:21 +0100

    2015 day 4 part 2

--- 2015/4/main.py
+++ 2015/4/main.py
@@ -2,8 +2,8 @@ import hashlib
 
 key = 'iwrupvqb'
 
-for i in range(1, 1024*1025):
+for i in range(1, 1024*1025*100):
     hashed = hashlib.md5((key + str(i)).encode('utf-8')).hexdigest()
-    if hashed[0:5] == '00000':
+    if hashed[0:6] == '000000':
         print(i)
         break
