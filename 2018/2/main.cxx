c86803489fb920182472bc5c56349efeb364a3ee
Geert Ijewski <gm.ijewski@web.de>
2020-12-06 17:27:15 +0100

    2018-12-02 part 2
    Start with:
    g++ -std=c++17 -o /tmp/aoc %; and /tmp/aoc ~/code/adventofcode/2018/2/input
    
    I somehow thought I had to submit all unique characters shared between
    those two strings...

--- 2018/2/main.cxx
+++ 2018/2/main.cxx
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <cassert>
 #include <array>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
 
 bool countDuplicates(std::string str, int expectedForAny) {
 	std::array<int, 26> count = {0};
@@ -16,15 +20,44 @@ bool countDuplicates(std::string str, int expectedForAny) {
 
 int main(int argc, char** argv) {
 	std::ifstream f(argv[1]);
+	std::vector<std::string> allLines;
 	std::string line;
 
-	auto totalDouble = 0;
-	auto totalTriple = 0;
 	while(std::getline(f, line)) {
-		totalDouble += countDuplicates(line, 2);
-		totalTriple += countDuplicates(line, 3);
+		allLines.push_back(line);
 	}
 
-	std::cout << totalDouble * totalTriple << std::endl;
+	for(auto& id: allLines) {
+		auto res = std::find_if(allLines.begin(), allLines.end(), [=](const std::string elem) {
+				std::array<char, 26> difference = {0};
+				assert(id.length() == elem.length() && id.length() == 26);
+
+				for(auto i = 0; i < id.length(); i++) {
+					difference[i] = (id[i] - elem[i]) != 0;
+				}
+
+				return std::accumulate(difference.begin(), difference.end(), 0) == 1;
+			});
+		if(res == std::end(allLines)) {
+			continue;
+		}
+
+		std::cout << id << " ~ " << *res << std::endl;
+
+
+		/*
+		std::set<char> a(id.begin(), id.end());
+		std::set<char> b((*res).begin(), (*res).end());
+		std::string sharedCharacters;
+		std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(sharedCharacters));
+
+		std::cout << " --> "  << sharedCharacters << std::endl;
+		*/
+		const auto[ firstDiffIt, _ ] = std::mismatch(id.begin(), id.end(), (*res).begin(), (*res).end());
+		id.erase(firstDiffIt);
+		std::cout << id << std::endl;
+
+		break;
+	}
 }
 
