430df635c6e3fe397ee3ed24b2e080210d26575e
Geert Ijewski <gm.ijewski@web.de>
2020-12-12 20:15:12 +0100

    2020-12-09 part 2

--- 2020/9/main.py
+++ 2020/9/main.py
@@ -2,16 +2,26 @@ sequence = []
 for line in open('input'):
     sequence.append(int(line))
 
-for j in range(25, 0, -1):
-    for k in range(j, 0, -1):
-        print(25 - j, sequence[25  -j], 25 - j + k, sequence[25 - j + k])
-
 for i in range(25, len(sequence)):
     combinations = []
     for j in range(25, 0, -1):
         for k in range(j, 0, -1):
             combinations.append(sequence[i - j] + sequence[i - j + k])
     if sequence[i] not in combinations:
-        print(combinations)
-        print(i, sequence[i])
+        answer = sequence[i]
+        maxIndex = i
         break
+
+for i in range(maxIndex):
+    res = minNum = maxNum  = sequence[i]
+    for j in range(i+1, maxIndex):
+        if sequence[j] < minNum:
+            minNum = sequence[j]
+        if sequence[j] > maxNum:
+            maxNum = sequence[j]
+
+        res += sequence[j]
+        if res == answer:
+            print("part 2 ", minNum + maxNum, minNum, maxNum)
+        elif res > answer:
+            break
