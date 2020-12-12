9842017a154b97db18e891b0d7f8d3acb136931e
Geert Ijewski <gm.ijewski@web.de>
2020-12-12 19:26:27 +0100

    2020-12-09 part 2

--- 2020/8/main.py
+++ 2020/8/main.py
@@ -4,18 +4,44 @@ for line in open('input'):
     ins, val = line.split()
     instructions.append((ins, val))
 
-visitedInstructions = set()
-accumulator = 0
-ip = 0
-while ip not in visitedInstructions:
-    visitedInstructions |= set([ip])
+def run(instructions):
+    visitedInstructions = set()
+    accumulator = 0
+    ip = 0
+    while ip not in visitedInstructions:
+        visitedInstructions |= set([ip])
 
-    ins, val = instructions[ip]
+        ins, val = instructions[ip]
+        if ins == 'nop':
+            ip += 1
+        elif ins == 'acc':
+            accumulator += int(val)
+            ip += 1
+        else:
+            ip += int(val)
+
+        if ip >= len(instructions):
+            return (accumulator, True)
+
+    return (accumulator, False)
+
+for x in range(len(instructions)):
+    ins, val = instructions[x]
     if ins == 'nop':
-        ip += 1
-    elif ins == 'acc':
-        accumulator += int(val)
-        ip += 1
+        copy = instructions.copy()
+        copy[x] = ('jmp', val)
+        
+        res, success = run(copy)
+        if success:
+            print(res)
+            break
+    elif ins == 'jmp':
+        copy = instructions.copy()
+        copy[x] = ('nop', val)
+        
+        res, success = run(copy)
+        if success:
+            print(res)
+            break
     else:
-        ip += int(val)
-print(accumulator)
+        continue
