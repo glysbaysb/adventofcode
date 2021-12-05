b54ea74597efdaf109f51ca5879f0a6cda6c1ec6
Geert Ijewski <gm.ijewski@web.de>
2021-12-05 11:57:26 +0100

    2021-12-04 p2

--- 2021/4/main.py
+++ 2021/4/main.py
@@ -21,26 +21,36 @@ for line in open('input'):
     boards[board].append([(int(x), False) for x in line.strip().replace("  ", ' ').split(' ')])
 # set
 for number in numbers:
+    print(number, len(boards))
     for board in range(0, len(boards)):
         for y in range(0, len(boards[board])):
             for x in range(0, len(boards[board][y])):
                 if boards[board][y][x][0] == number:
                     boards[board][y][x] = (number, True)
 
+    for board in boards:
         # after every set, check
         # first rows
-        for y in range(0, len(boards[board])):
+        checkCol = True # skip column check if we removed the whole board
+        for y in range(0, len(board)):
             foundTrue = 0
-            for x in range(0, len(boards[board][y])):
-                if boards[board][y][x][1]:
+            for x in range(0, len(board[y])):
+                if board[y][x][1]:
                     foundTrue = foundTrue + 1
             if foundTrue == 5:
-                print("hooray row", unmarked(boards[board]) * number)
+                print("hooray row", unmarked(board) * number)
+                boards.remove(board)
+                checkCol = False
+                break
+        if not checkCol: # if board has been removed, no need to check columns
+            continue
         # then columns
         for x in range(0, 5):
             foundTrue = 0
             for y in range(0, 5):
-                if boards[board][y][x][1]:
+                if board[y][x][1]:
                     foundTrue = foundTrue + 1
             if foundTrue == 5:
-                print("hooray col", unmarked(boards[board]) * number)
+                print("hooray col", unmarked(board) * number)
+                boards.remove(board)
+                break
