30eac6224f95ad0b1c9d3c0b849127b376a18784
Geert Ijewski <gm.ijewski@web.de>
2022-12-03 22:58:36 +0100

    p2

--- 2022/3/main.py
+++ 2022/3/main.py
@@ -1,13 +1,22 @@
 counter = 0
+lines = []
 for line in open('input'):
-    p1 = line[:len(line)//2]
-    p2 = line[len(line)//2:]
+    if len(line) > 2:
+        lines.append(line.strip())
+i = 0
+while i < len(lines):
+    l1 = lines[i]
+    l2 = lines[i+1]
+    l3 = lines[i+2]
 
-    res = (set(p1) & set(p2)).pop()
+
+    res = (set(l1) & set(l2) & set(l3)).pop()
     if res >= 'a' and res <= 'z':
         counter = counter + ord(res) - ord('a') + 1
     else:
         counter = counter + ord(res) - ord('A') + 27
+
+    i = i + 3
 print(counter)
         
 
