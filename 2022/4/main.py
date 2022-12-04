e3511ef930c1513d56e9f9d06f14c33bdf917fc6
Geert Ijewski <gm.ijewski@web.de>
2022-12-04 16:10:48 +0100

    p2

--- 2022/4/main.py
+++ 2022/4/main.py
@@ -4,10 +4,7 @@ for line in open('input'):
     e1_s, e1_e = e1.split('-')
     e2_s, e2_e = e2.split('-')
 
-    if int(e1_s) >= int(e2_s) and int(e1_e) <= int(e2_e):
-        counter = counter + 1
-        print(line, "in")
-    elif int(e2_s) >= int(e1_s) and int(e2_e) <= int(e1_e):
+    if int(e1_s) <= int(e2_e) and int(e1_e) >= int(e2_s):
         counter = counter + 1
         print(line, "in")
     else:
